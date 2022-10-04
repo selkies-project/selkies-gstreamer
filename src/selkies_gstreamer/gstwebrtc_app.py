e6a4d063b00f2c8172636cd0d6da9e02cc34306c
ehfd <8457324+ehfd@users.noreply.github.com>
2022-10-04 10:44:17 +0000

    Handle x264enc threads automatically, rollback if there is a reason

--- src/selkies_gstreamer/gstwebrtc_app.py
+++ src/selkies_gstreamer/gstwebrtc_app.py
@@ -307,7 +307,6 @@ class GSTWebRTCApp:
 
             # encoder
             x264enc = Gst.ElementFactory.make("x264enc", "x264enc")
-            x264enc.set_property("threads", 4)
             x264enc.set_property("bframes", 0)
             x264enc.set_property("key-int-max", 0)
             x264enc.set_property("byte-stream", True)
