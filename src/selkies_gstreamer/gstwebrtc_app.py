52631b92240ec86446a27d86e1dd73f6ae729029
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2022-11-28 18:29:10 +0900

    Prevent errors for SDP parsing in recent GStreamer versions

--- src/selkies_gstreamer/gstwebrtc_app.py
+++ src/selkies_gstreamer/gstwebrtc_app.py
@@ -657,8 +657,7 @@ class GSTWebRTCApp:
         if sdp_type != 'answer':
             raise GSTWebRTCAppError('ERROR: sdp type was not "answer"')
 
-        _, sdpmsg = GstSdp.SDPMessage.new()
-        GstSdp.sdp_message_parse_buffer(bytes(sdp.encode()), sdpmsg)
+        _, sdpmsg = GstSdp.SDPMessage.new_from_text(sdp)
         answer = GstWebRTC.WebRTCSessionDescription.new(
             GstWebRTC.WebRTCSDPType.ANSWER, sdpmsg)
         promise = Gst.Promise.new()
