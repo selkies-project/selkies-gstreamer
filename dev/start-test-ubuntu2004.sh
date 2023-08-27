d5ca34fbea3ec465f12930d3f4de3dc0c7945112
ehfd <8457324+ehfd@users.noreply.github.com>
2023-08-27 19:03:48 +0900

    Add helper scripts for building and testing

---
+++ dev/start-test-ubuntu2004.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(readlink -f $(dirname $0))
+
+# Change this to set the ubuntu version
+UBUNTU_VERSION=20.04
+
+(
+    cd ${SCRIPT_DIR?}/.. && \
+    GSTREAMER_BASE_IMAGE=gstreamer TEST_IMAGE=selkies-gstreamer-example:latest-ubuntu${UBUNTU_VERSION} UBUNTU_RELEASE=${UBUNTU_VERSION} docker-compose run --service-ports test
+)
