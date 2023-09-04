273e7adb78e983a3a9df6a4485e6b59c5f76e90d
Dan Isla <dan.isla@gmail.com>
2023-09-04 08:46:14 -0600

    import of devcontainer

---
+++ .devcontainer/features/desktop-selkies/src/install-desktop-environment.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+
+function install_fluxbox() {
+    sudo apt-get update
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+            fluxbox \
+            terminator
+}
+
+function install_xfce() {
+    sudo apt-get update
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+            xfce4 \
+            xfce4-terminal \
+            breeze-cursor-theme
+
+    # Configure desktop environment
+    sudo apt remove -y \
+            xfce4-screensaver
+
+    sudo ln -fs /etc/xfce4/defaults.list /usr/share/applications/defaults.list
+}
+
+function install_kde() {
+    sudo apt-get update
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+            kde-plasma-desktop \
+            konsole \
+            breeze-cursor-theme
+}
+
+function install_ubuntu_desktop() {
+    sudo apt-get update
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
+            ubuntu-desktop \
+            gnome-terminal \
+            breeze-cursor-theme
+}
+
+case ${1,,} in
+    fluxbox)
+        install_fluxbox
+        ;;
+    xfce)
+        install_xfce
+        ;;
+    kdeplasma)
+        install_kde
+        ;;
+    ubuntu)
+        install_ubuntu_desktop
+        ;;
+    *)
+        echo "ERROR: unsupported desktop environment: $1"
+        exit 1
+        ;;
+esac
---
+++ .devcontainer/features/desktop-selkies/src/start-selkies.sh
@@ -0,0 +1,72 @@
+export DISPLAY=:0
+unset WAYLAND_DISPLAY
+export XSERVER=${XSERVER:-XVFB}
+
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+
+function cleanup() {
+    kill -9 $(pidof turnserver) 1>/dev/null 2>&1|| true
+    pgrep -af '.*selkies-gstreamer.*' | cut -d' ' -f1 | xargs kill -9 1>/dev/null 2>&1|| true
+    pgrep -afi '.*xfce4.*' | cut -d' ' -f1 | xargs kill -9 1>/dev/null 2>&1|| true
+    pgrep -afi '.*fluxbox.*' | cut -d' ' -f1 | xargs kill -9 1>/dev/null 2>&1|| true
+    sudo /usr/bin/pulseaudio -k 1>/dev/null 2>&1  || true
+    kill -9 $(pidof Xvfb) 1>/dev/null 2>&1 || true
+    exit
+}
+trap cleanup SIGINT SIGKILL EXIT
+
+# Start Xvfb Xserver
+if [[ ${XSERVER} == "XVFB" ]]; then
+    Xvfb -screen :0 8192x4096x24 +extension RANDR +extension GLX +extension MIT-SHM -nolisten tcp -noreset -shmem 2>&1 >/tmp/Xvfb.log &
+fi
+
+# Wait for X11 to start
+echo "Waiting for X socket"
+until [ -S "/tmp/.X11-unix/X${DISPLAY/:/}" ]; do sleep 1; done
+echo "X socket is ready"
+
+# Disable screen saver
+xset s off
+# Disable screen blanking
+xset s noblank
+# Disable power management
+xset -dpms
+
+# Start pulse audio server
+export PULSE_SERVER=tcp:127.0.0.1:4713
+sudo /usr/bin/pulseaudio -k >/dev/null 2>&1
+sudo /usr/bin/pulseaudio --daemonize --system --verbose --log-target=file:/tmp/pulseaudio.log --realtime=true --disallow-exit -L 'module-native-protocol-tcp auth-ip-acl=127.0.0.0/8 port=4713 auth-anonymous=1'
+
+# Start desktop environment
+case ${DESKTOP:-XFCE} in
+    FLUXBOX)
+        startfluxbox &
+        ;;
+    XFCE)
+        xfce4-session &
+        ;;
+    *)
+        echo "WARN: Unsupported DESTKOP: '${DESKTOP}'"
+        ;;
+esac
+
+# Source gstreamer environment
+source /opt/gstreamer/gst-env
+
+# Start turnserver
+${SCRIPT_DIR}/start-turnserver.sh &
+
+# Start Selkies
+selkies-gstreamer-resize 1920x1080
+selkies-gstreamer \
+    --addr="0.0.0.0" \
+    --port="${WEB_PORT:-6080}" \
+    --metrics_port=${WEBRTC_METRICS_PORT:-19090} \
+    --cursor_size=${WEBRTC_CURSOR_SIZE:-"-1"} \
+    --enable_resize=${WEBRTC_ENABLE_RESIZE:-true} \
+    --turn_host=${TURN_HOST:-localhost} \
+    --turn_port=${TURN_PORT:-3478} \
+    --turn_username=${TURN_USERNAME:-selkies} \
+    --turn_password=${TURN_PASSWORD:-selkies} \
+    --turn_protocol=${TURN_PROTOCOL:-tcp} \
+    $@
