f598267ed6e3d9de0aeed9185d2aed493c6bc62b
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-06-18 15:06:36 +0900

    Work around subpackage in Conda

--- addons/conda/build/build.sh
+++ addons/conda/build/build.sh
@@ -32,7 +32,7 @@ rm -rf build
 popd
 
 # Install Selkies python-xlib fork
-${PYTHON} -m pip install -vv "python-xlib @ https://github.com/selkies-project/python-xlib/archive/master.zip"
+${PYTHON} -m pip install -vv --no-build-isolation "https://github.com/selkies-project/python-xlib/archive/master.zip"
 # Install Selkies Python components
 ${PYTHON} -m pip install -vv --no-deps --no-build-isolation "${SELKIES_SOURCE}/${PYPI_PACKAGE}-${PACKAGE_VERSION}-py3-none-any.whl"
 # Install web interface components
