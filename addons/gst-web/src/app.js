cf470551d2abad66e856f726409e7b48bf72fa51
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-06-22 20:59:06 +0900

    Test minimum latency target interval

--- addons/gst-web/src/app.js
+++ addons/gst-web/src/app.js
@@ -545,7 +545,7 @@ webrtc.onconnectionstatechange = (state) => {
                 } else {
                     receiver.jitterBufferTarget = receiver.jitterBufferDelayHint = receiver.playoutDelayHint = 0;
                 }
-            }, 10);
+            }, 5);
         });
     }
     if (videoConnected === "connected" && audioConnected === "connected") {
@@ -569,7 +569,7 @@ audio_webrtc.onconnectionstatechange = (state) => {
                 } else {
                     receiver.jitterBufferTarget = receiver.jitterBufferDelayHint = receiver.playoutDelayHint = 0;
                 }
-            }, 10);
+            }, 5);
         });
     }
     if (audioConnected === "connected" && videoConnected === "connected") {
