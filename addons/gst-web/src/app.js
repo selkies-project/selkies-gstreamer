308734d09113d15dd85c02faa8d9be11621aa941
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-06-22 21:56:31 +0900

    Jitterbuffer Target to 10 ms

--- addons/gst-web/src/app.js
+++ addons/gst-web/src/app.js
@@ -545,7 +545,7 @@ webrtc.onconnectionstatechange = (state) => {
                 } else {
                     receiver.jitterBufferTarget = receiver.jitterBufferDelayHint = receiver.playoutDelayHint = 0;
                 }
-            }, 5);
+            }, 10);
         });
     }
     if (videoConnected === "connected" && audioConnected === "connected") {
@@ -569,7 +569,7 @@ audio_webrtc.onconnectionstatechange = (state) => {
                 } else {
                     receiver.jitterBufferTarget = receiver.jitterBufferDelayHint = receiver.playoutDelayHint = 0;
                 }
-            }, 5);
+            }, 10);
         });
     }
     if (audioConnected === "connected" && videoConnected === "connected") {
