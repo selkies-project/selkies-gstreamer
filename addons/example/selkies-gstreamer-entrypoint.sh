2474a482df4d836486f518339c26800f6431fbde
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-06-26 00:31:12 +0900

    Minor update

--- addons/example/selkies-gstreamer-entrypoint.sh
+++ addons/example/selkies-gstreamer-entrypoint.sh
@@ -61,6 +61,8 @@ server {
         proxy_set_header        Connection \"upgrade\";
 
         proxy_set_header        Host \$host;
+        proxy_set_header        X-Real-IP \$remote_addr;
+        proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header        X-Forwarded-Proto \$scheme;
 
         proxy_http_version      1.1;
@@ -79,6 +81,8 @@ server {
         proxy_set_header        Connection \"upgrade\";
 
         proxy_set_header        Host \$host;
+        proxy_set_header        X-Real-IP \$remote_addr;
+        proxy_set_header        X-Forwarded-For \$proxy_add_x_forwarded_for;
         proxy_set_header        X-Forwarded-Proto \$scheme;
 
         proxy_http_version      1.1;
