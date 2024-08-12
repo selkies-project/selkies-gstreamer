40aab8d9d5870b872edda7c86d5167ece329362b
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2024-08-12 22:51:30 +0900

    Fix Conda

--- addons/conda/build/build.sh
+++ addons/conda/build/build.sh
@@ -5,7 +5,6 @@ set -e -x
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-pushd gstreamer
 mkdir -pm755 build
 pushd build
 
