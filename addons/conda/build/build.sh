9e5028c5ca0907ab54596dbde79077174503c469
Seungmin Kim <8457324+ehfd@users.noreply.github.com>
2025-03-19 23:56:27 +0900

    Fix build

--- addons/conda/build/build.sh
+++ addons/conda/build/build.sh
@@ -8,9 +8,9 @@ set -e -x
 mkdir -pm755 build
 pushd build
 
-export XDG_DATA_DIRS="${XDG_DATA_DIRS}:${PREFIX}/share"
-export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${PREFIX}/lib/pkgconfig"
-export PKG_CONFIG="${BUILD_PREFIX}/bin/pkg-config"
+export XDG_DATA_DIRS="${XDG_DATA_DIRS}:${PREFIX}/share:${BUILD_PREFIX}/share"
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:${PREFIX}/lib/pkgconfig:${BUILD_PREFIX}/lib/pkgconfig"
+export PKG_CONFIG="$(which pkg-config)"
 
 # Build and install GStreamer
 export EXTRA_FLAGS="${EXTRA_FLAGS}"
